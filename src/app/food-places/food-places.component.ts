9c766d81137ffc6941fd03556b0da5b3754f5936
laurenhay1 <lauren.hayes87@student.qcc.cuny.edu>
2020-12-18 19:52:11 -0500

    4 files modified, 2 added

---
+++ src/app/food-places/food-places.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-places',
+  templateUrl: './food-places.component.html',
+  styleUrls: ['./food-places.component.css']
+})
+export class FoodPlacesComponent implements OnInit {
+
+  constructor() { }
+
+isOffcanvas = true;
+
+  ngOnInit() {
+  }
+
+}
